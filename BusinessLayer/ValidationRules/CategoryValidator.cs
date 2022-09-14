0cc0861304ea78c14b515f86afc010172aa75017
Mustafa Burak Gürener <mb.gurener@gmail.com>
2022-09-14 16:22:33 +0300

    Add project files.

---
+++ BusinessLayer/ValidationRules/CategoryValidator.cs
@@ -0,0 +1,22 @@
+﻿using EntityLayer.Concrete;
+using FluentValidation;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BusinessLayer.ValidationRules
+{
+    public class CategoryValidator:AbstractValidator<Category>
+    {
+        public CategoryValidator()
+        {
+            RuleFor(x => x.CategoryName).NotEmpty().WithMessage("Kategori Adı Boş Geçilşemez");
+            RuleFor(x => x.CategoryName).MaximumLength(20).WithMessage("Kategori Adı En Fazla 20 Karakter Olabilir.");
+            RuleFor(x => x.CategoryName).MinimumLength(5).WithMessage("Kategori Adı En Az 5 Karakter Olabilir.");
+
+
+        }
+    }
+}
---
+++ DataAccessLayer/Repositories/GenericRepository.cs
@@ -0,0 +1,51 @@
+﻿using DataAccessLayer.Abstract;
+using DataAccessLayer.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccessLayer.Repositories
+{
+    public class GenericRepository<T> : IGenericDAL<T> where T : class
+    {
+        Context context = new Context();
+
+        DbSet<T> _object;
+
+        public GenericRepository()
+        {
+            _object = context.Set<T>();
+        }
+
+        public void Delete(T item)
+        {
+          _object.Remove(item);
+        }
+
+        public T GetById(int id)
+        {
+             
+            return _object.Find(id);
+        }
+
+        public List<T> GetList()
+        {
+            return _object.ToList();
+
+        }
+
+        public void Insert(T item)
+        {
+            _object.Add(item);
+            context.SaveChanges();
+        }
+
+        public void Update(T item)
+        {
+            context.SaveChanges();
+        }
+    }
+}
